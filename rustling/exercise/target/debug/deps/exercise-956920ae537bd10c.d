ef162796a2f61d4c8add60425ea7a81a0adf04de
rahulpatle-sol <patler044@gmail.com>
2025-12-28 15:27:30 +0530

    structured everhing

---
+++ rustling/exercise/target/debug/deps/exercise-956920ae537bd10c.d
@@ -0,0 +1,5 @@
+/home/rahul-patle/rustling/exercise/target/debug/deps/exercise-956920ae537bd10c.d: src/main.rs
+
+/home/rahul-patle/rustling/exercise/target/debug/deps/libexercise-956920ae537bd10c.rmeta: src/main.rs
+
+src/main.rs:
